b7ae1e2836a92d0eb0ef3722a98c877e032d9a4d
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-03-10 21:40:47 -0600

    adds heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -69,6 +69,14 @@ export class HeroService {
     );
   }
 
+  /** POST: add a new hero to the server */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
